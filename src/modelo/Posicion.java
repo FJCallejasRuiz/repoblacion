c13cf67db7c1164c41ce2bdf5bc4f7c8e912544c
fran_ <fran_@LAPTOP-FO9M2E4T>
2023-01-29 19:32:31 +0100

    Commit tercero.
    El test debio ser movido para su verificación. al final serán borrados.

--- src/modelo/Posicion.java
+++ src/modelo/Posicion.java
@@ -1,6 +1,64 @@
 package modelo;
 
+import java.text.DecimalFormat;
+
 public class Posicion {
+	private double x, y;
 
+	public Posicion(double x, double y) {
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public Posicion(Posicion p) {
+		if (p == null) {
+			throw new NullPointerException("ERROR: No se puede copiar una posición nula.");
+		}
+		x = p.getX();
+		y = p.getY();
+	}
+	
+	public double distancia(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No se puede calcular la distancia a una posición nula.");
+		}if (this.getX()== posicion.getX() && this.getY()== posicion.getY()) {
+			return 0;
+		}
+		double distanciaADar = Math.sqrt(Math.pow((posicion.getX()-this.getX()), 2)+Math.pow((posicion.getY()-this.getY()), 2));
+		return distanciaADar;
+		
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	private void setX(double x) {
+		if (x < 0) {
+			throw new IllegalArgumentException("ERROR: No se puede un valor negativo.");
+		}
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	private void setY(double y) {
+		if (y < 0) {
+			throw new IllegalArgumentException("ERROR: No se puede un valor negativo.");
+		}
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat tresDigitos= new DecimalFormat("#.000");
+		return "x=" + tresDigitos.format(x) + ", y=" + tresDigitos.format(y);
+	}
+
+	
+	
+	
+	
 }
-
