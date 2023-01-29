90aafaf304157349f42e4cc889bae8f89f373440
fran_ <fran_@LAPTOP-FO9M2E4T>
2023-01-29 18:31:00 +0100

    segundo commit
    fallo a la hora dei ntentar meter commit.

--- src/modelo/Especie.java
+++ src/modelo/Especie.java
@@ -1,5 +1,18 @@
 package modelo;
 
 public enum Especie {
+	ALAMO("Álamo"), ENCINA("Encina"), CASTANO("Castaño"), CIPRES("Ciprés"), PINO("Pino"), ROBLE("Roble"),
+	OLIVO("Olivo");
 
+	public String cadenaAMostrar;
+
+	private Especie(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	@Override
+	public String toString() {
+		return cadenaAMostrar;
+
+	}
 }
